90bf85a55aee06663362099a9f344b656a081fcd
Fedora-Laptop <chasefiore@gmail.com>
2026-04-19 18:02:28 -0400

    Wrangler 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  // No adapter needed for a static portfolio!
   output: 'static',
-  adapter: cloudflare({
-    // This tells Astro NOT to try and find a KV database for sessions
-    platformProxy: {
-      enabled: true,
-    },
-    imageService: 'compile'
-  }),
 });
